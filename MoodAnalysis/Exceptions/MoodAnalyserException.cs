47ab1789df48a583670119818eccc59d4a09c1e4
pratikshatamadalge <pratikshatamadalge21@gmail.com>
2020-07-07 08:39:57 +0530

    Added: enum for Exception Handling

--- MoodAnalysis/Exceptions/MoodAnalyserException.cs
+++ MoodAnalysis/Exceptions/MoodAnalyserException.cs
@@ -7,12 +7,12 @@ namespace MoodAnalysis
     public class MoodAnalyserException : Exception
     {
        public string message;
-       public enum TypeOfException
-       {
-            NULL_POINTER_EXCEPTION,EMPTY_STRING_EXCEPTION, NO_CLASS_FOUND, NO_CONSTRUCTOR_FOUND,OBJECT_NOT_CREATED,
+
+        public enum TypeOfException
+        {
+            NULL_POINTER_EXCEPTION, EMPTY_STRING_EXCEPTION, NO_CLASS_FOUND, NO_CONSTRUCTOR_FOUND, OBJECT_NOT_CREATED,
             NO_CONSTRUCTOR_WITH_SUCH_PARAMETER, NO_METHOD_FOUND, NO_SUCH_FIELD
         }
-
 
         public TypeOfException exceptionType;
         public MoodAnalyserException(TypeOfException typeOfException, string message) : base(message)
